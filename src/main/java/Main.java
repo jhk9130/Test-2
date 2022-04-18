6c66f0a9509f083f663f86aae054c2820b5c9912
jhk9130 <hyunk66@gmail.com>
2022-04-18 16:59:53 -0400

    Update Test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("This is my second repository. Made another branch!");
+        System.out.println("This is pretty neat!");
     }
 }
