6bb3541883933f36e12c63b206f062cdd9a66503
jhk9130 <hyunk66@gmail.com>
2022-04-18 16:47:05 -0400

    memGit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("This is my second repository");
+        System.out.println("This is my second repository. Made another branch.");
     }
 }
